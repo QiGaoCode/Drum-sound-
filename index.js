9bcc515f95410e55be4134ff1d1265aa93e2d1ec
QiGaoCode <45082230+QiGaoCode@users.noreply.github.com>
2018-11-22 16:57:52 -0500

    Drum sound

---
+++ index.js
@@ -0,0 +1,25 @@
+
+
+
+
+
+
+
+
+  
+    //
+    // function change() {
+    //   document.getElementById("title").style.color = "blue";
+    // } -->
+
+
+// document.querySelector("button").addEventListener("click", changeToPurple);
+//
+// // document.addEventListener("keydown", function() {
+// //   alert("keydown");
+// // });
+//
+// function changeToPurple() {
+//   var h1 = document.querySelector("h1"); //SELECT
+//   h1.style.color = "purple"; //MANIPULATE
+// }
